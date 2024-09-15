0ca29cee9f03b9cbc4dfdcaf4cd5ef2c6b8143b0
Wishbert Mahlong <mmahlong@mrpricegroup.com>
2024-09-15 23:15:21 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='PCB_ML_PROJECT',
+    version='0.0.1',
+    author='Wishbert',
+    author_email='mwishbert@gmail.com',
+    packages=find_packages()
+)
